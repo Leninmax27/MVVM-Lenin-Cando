3facc2cd99c62391bea0ed2ee1db645335466d28
MSI <MSI@DESKTOP-8EEL6TR>
2024-07-09 15:25:24 -0500

    Agregar archivos de proyecto.

---
+++ ViewModels/LCAboutViewModel.cs
@@ -0,0 +1,21 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using System.Windows.Input;
+
+namespace Notes.ViewModels;
+
+internal class LCAboutViewModel
+{
+    public string Title => AppInfo.Name;
+    public string Version => AppInfo.VersionString;
+    public string MoreInfoUrl => "https://aka.ms/maui";
+    public string Message => "Nuestro cerebro es como un procesador";
+    public ICommand ShowMoreInfoCommand { get; }
+
+    public LCAboutViewModel()
+    {
+        ShowMoreInfoCommand = new AsyncRelayCommand(ShowMoreInfo);
+    }
+
+    async Task ShowMoreInfo() =>
+        await Launcher.Default.OpenAsync(MoreInfoUrl);
+}
---
+++ ViewModels/LCNotesViewModel.cs
@@ -0,0 +1,57 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using Notes.Models;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace Notes.ViewModels;
+
+internal class LCNotesViewModel : IQueryAttributable
+{
+    public ObservableCollection<ViewModels.LCNoteViewModel> AllNotes { get; }
+    public ICommand NewCommand { get; }
+    public ICommand SelectNoteCommand { get; }
+
+    public LCNotesViewModel()
+    {
+        AllNotes = new ObservableCollection<ViewModels.LCNoteViewModel>(Models.Note.LoadAll().Select(n => new LCNoteViewModel(n)));
+        NewCommand = new AsyncRelayCommand(NewNoteAsync);
+        SelectNoteCommand = new AsyncRelayCommand<ViewModels.LCNoteViewModel>(SelectNoteAsync);
+    }
+
+    private async Task NewNoteAsync()
+    {
+        await Shell.Current.GoToAsync(nameof(Views.NotePage));
+    }
+
+    private async Task SelectNoteAsync(ViewModels.LCNoteViewModel note)
+    {
+        if (note != null)
+            await Shell.Current.GoToAsync($"{nameof(Views.NotePage)}?load={note.Identifier}");
+    }
+
+    void IQueryAttributable.ApplyQueryAttributes(IDictionary<string, object> query)
+    {
+        if (query.ContainsKey("deleted"))
+        {
+            string noteId = query["deleted"].ToString();
+            LCNoteViewModel matchedNote = AllNotes.Where((n) => n.Identifier == noteId).FirstOrDefault();
+
+            // If note exists, delete it
+            if (matchedNote != null)
+                AllNotes.Remove(matchedNote);
+        }
+        else if (query.ContainsKey("saved"))
+        {
+            string noteId = query["saved"].ToString();
+            LCNoteViewModel matchedNote = AllNotes.Where((n) => n.Identifier == noteId).FirstOrDefault();
+
+            // If note is found, update it
+            if (matchedNote != null)
+                matchedNote.Reload();
+
+            // If note isn't found, it's new; add it.
+            else
+                AllNotes.Add(new LCNoteViewModel(Note.Load(noteId)));
+        }
+    }
+}
